407c4e6d2afc2260bd2f64ac5250f6900d0c5a50
nicola <nickhannibal@gmail.com>
2021-08-09 17:48:43 +0900

    Initial commit

---
+++ src/main/java/org/miniats/jobopeningservice/JobOpeningServiceApplication.java
@@ -0,0 +1,13 @@
+package org.miniats.jobopeningservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobOpeningServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobOpeningServiceApplication.class, args);
+	}
+
+}
